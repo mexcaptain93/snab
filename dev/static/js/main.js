5237f48ec02417bb7567cd7efbe71138ce1c5820
mexcaptain <mexcaptain@gmail.com>
2020-06-12 00:22:36 +0300

    Index page

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
     svg4everybody({});
+    vendorSlider();
 });
+
+function vendorSlider() {
+    $('.js-vendor-slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<a href="#" class="vendors__prev"></a>',
+        nextArrow: '<a href="#" class="vendors__next"></a>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        }]
+    });
+}
