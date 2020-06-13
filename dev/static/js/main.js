1fa38a6a38aae893e47426c04f4959bde0a72dab
mexcaptain <mexcaptain@gmail.com>
2020-06-13 12:05:18 +0300

    Footer feedback

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     svg4everybody({});
     vendorSlider();
+    mobileMenu();
+    feedbackFooter();
+
 });
 
-function vendorSlider() {
+function vendorSlider () {
     $('.js-vendor-slider').slick({
         infinite: true,
         slidesToShow: 6,
@@ -20,4 +23,43 @@ function vendorSlider() {
             }
         }]
     });
+}
+
+function mobileMenu () {
+    var opener = $('.js-burger'),
+        menu = $('.js-menu');
+
+    opener.on('click', function (e) {
+        e.preventDefault();
+        menu.slideDown();
+    });
+
+    $(document).click(function(event) {
+        target = $(event.target);
+        if(!target.closest(menu).length &&
+            target[0] != opener[0] && menu.is(":visible"))  {
+            menu.slideUp();
+        }
+    });
+
+}
+
+function feedbackFooter () {
+    var opener = $('.js-feedback-opener'),
+        feedback = $('.js-feedback');
+
+    opener.on('click', function (e) {
+        e.preventDefault();
+        feedback.show();
+    });
+
+    $(document).click(function(event) {
+        target = $(event.target);
+        console.log(feedback, target)
+
+        if(!target.closest(feedback).length && target[0] != opener[0] && feedback.is(":visible"))  {
+            feedback.hide();
+        }
+    });
+
 }
